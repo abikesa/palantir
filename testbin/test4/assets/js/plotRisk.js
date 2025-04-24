38732017b4d000068ef3e23933dc49f7b73faabd
abikesa <abikesa.sh@gmail.com>
2025-04-24 16:31:54 -0400

    Update documentation

---
+++ testbin/test4/assets/js/plotRisk.js
@@ -0,0 +1,20 @@
+function plotRisk(timePoints, mortalityRisk, esrdRisk) {
+
+    // Layout for the plot
+    const layout = {
+        title: 'Risk Over Time',
+        xaxis: {
+            title: 'Time (years)',
+            showgrid: true
+        },
+        yaxis: {
+            title: 'Risk (%)',
+            range: [0, 100],
+            showgrid: true
+        }
+    };
+
+    // Create the plot
+    Plotly.newPlot('risk-graph', data, layout);
+}# flick 20250424193541-6t3v
+# flick 20250424195736-oglJ
